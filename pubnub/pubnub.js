d152850359b668f3b6a8c6cf0d8c150ab76dbe87
Apadotech <79450260+Apadotech@users.noreply.github.com>
2021-02-26 15:59:08 +0800

    Create pubnub.js

---
+++ pubnub/pubnub.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>PubNub-Chat</title>
+</head>
+<body>
+
+  <div id="app"></div>
+
+  <script src="http://cdn.pubnub.com/pubnub-3.15.2.min.js"></script>
+  <script src="bundle.js"></script>
+</body>
+</html>
