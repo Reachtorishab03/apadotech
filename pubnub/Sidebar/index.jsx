e25b0a7a6d5fb1c894d96e330ec718639c3f14da
Apadotech <79450260+Apadotech@users.noreply.github.com>
2021-02-26 16:05:46 +0800

    Update index.jsx

--- pubnub/Sidebar/index.jsx
+++ pubnub/Sidebar/index.jsx
@@ -140,3 +140,79 @@ export default class Sidebar extends React.Component {
     );
   }
 }
+
+import React from "react";
+
+
+export default class MainSection extends React.Component {
+  render() {
+    return (
+      <div className="main">
+        <MessageSection
+          messages={this.props.messages}
+          pubnub={this.props.pubnub}
+         />
+        <InputSection />
+      </div>
+    )
+  }
+}
+
+/*****************
+MessageSection Component
+************************/
+
+class MessageSection extends React.Component {
+  render() {
+    if(this.props.messages.length > 0){
+      let messageList = this.props.messages[0]
+
+      //map over array of messages
+      let messages = messageList.map(function(message){
+        // Get current user
+        let user = this.props.pubnub.get_uuid()
+
+        //check if message is from current user
+        if(user == message.username){
+          return(
+            <div className='messageSent' key={message.date} >
+              <h3>{message.text}</h3>
+            </div>
+          )
+        }else {
+          return(
+            <div className="messageRecieved" key={message.date}>
+              <p>{message.username}</p>
+              <h3>{message.text}</h3>
+            </div>
+          )
+        }
+      }.bind(this));
+      return(
+        <div className="message-section">
+          {messages}
+        </div>
+      )
+    }
+    return(
+      <div className="message-section"></div>
+    )
+  }
+}
+
+/*****************
+InputSection Component
+************************/
+
+class InputSection extends React.Component {
+  render() {
+    return (
+      <div className="input-section">
+        <form>
+          <input id="text-input" type="text" placeholder="Hello World." />
+          <input id="submit-button" type="submit" />
+        </form>
+      </div>
+    )
+  }
+}
